362eaae78ca94d26daf9300791cc1dabebdb8869
Martin Damyanov <m.damyanov@student.tudelft.nl>
2024-05-05 19:08:25 +0200

    CHORE: make vitest work

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()],
-	test: {
-		coverage: {
-			enabled: true,
-			reporter: ['cobertura'],
-			provider: 'istanbul',
-			reportsDirectory: './reports/coverage',
-		},
-	}
+	plugins: [sveltekit(), purgeCss()]
 });
