bcf92da7b30eb4ea4de7c02205c125a315296578
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-05-06 23:11:24 +0200

    FEAT: dynamic tag component and index update for component folder

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,11 +2,6 @@
 
 // Components
 import Meta from "$lib/components/Meta.svelte";
-import Header from "$lib/components/Header.svelte";
-import Grid from "$lib/components/Grid.svelte";
-import SearchBar from "$lib/components/SearchBar.svelte";
-import UserHomePreview from "$lib/components/UserPreview/UserHomePreview.svelte";
-import UserMaterialView from "$lib/components/UserPreview/UserMaterialView.svelte";
-import UserPublishView from "$lib/components/UserPreview/UserPublishView.svelte";
-import UserSearch from "$lib/components/UserPreview/UserSearch.svelte";
-export {Meta, Header, Grid, SearchBar, UserHomePreview,UserPublishView,UserSearch,UserMaterialView}
+import Tag from "$lib/components/Tag.svelte";
+
+export {Meta, Tag}
