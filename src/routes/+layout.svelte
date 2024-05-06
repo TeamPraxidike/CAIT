1c952c600f5dda163adf8fbdf232a6a970789b48
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-06 22:55:19 +0200

    feat: grid component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
-    import {Header} from "$lib"
+    import {Grid, Header} from "$lib"
 
 
     hljs.registerLanguage('xml', xml); // for HTML
@@ -53,12 +53,11 @@
 
 <Header/>
 
-<main class="text-surface-900
-             grid grid-cols-4 gap-x-2 mx-4
-             md:grid-cols-8 md:gap-x-4 md:mx-8
-             lg:grid-cols-12 lg:gap-x-2 lg:mx-32
-             xl:mx-64">
-    <slot />
-</main>
+<div class="w-screen dark:text-surface-50 text-surface-900">
+    <Grid>
+        <slot />
+    </Grid>
+</div>
+
 
 <!--Footer-->
