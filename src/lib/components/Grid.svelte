78808529130c80790a83c1c0b3ce345b2fa6d587
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-06 22:55:19 +0200

    feat: grid component

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,6 @@
+<div class=" grid grid-cols-4 gap-x-2 mx-4
+             md:grid-cols-8 md:gap-x-4 md:mx-8
+             lg:grid-cols-12 lg:gap-x-2 lg:mx-32
+             xl:mx-64">
+    <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,16 @@
 </script>
 
 <Meta title="Home" description="CAIT" type="site" />
+
+<main>
+    <h1>Home</h1>
+	<p>Welcome to the home page.</p>
+	<p>Click on the links above to navigate to other pages.</p>
+	<p>Click on the links below to navigate to other pages.</p>
+	<nav>
+		<ul>
+			<li><a href="/about">About</a></li>
+			<li><a href="/contact">Contact</a></li>
+		</ul>
+	</nav>
+</main>
