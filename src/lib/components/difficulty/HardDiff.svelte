f7f7f782a39bae1b09dbdfa1e69ca382b63beaae
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-05-07 20:58:00 +0200

    FEAT: adding difficulty bars and selection

--- src/lib/components/difficulty/HardDiff.svelte
+++ src/lib/components/difficulty/HardDiff.svelte
@@ -1,6 +1,5 @@
-<div class="flex items-end justify-start space-x-0.5 w-5 h-5 ">
-    <div class="bg-error-400 w-1/4 h-1/2"></div>
-    <div class="bg-error-400  w-1/4 h-3/4"></div>
-    <div class="bg-error-400 w-1/4 h-full"></div>
+<div class="flex items-end justify-start space-x-0.5 w-4 h-4 md:w-7 md:h-7">
+    <div class="border dark:border-white border-black dark:border-opacity-50 border-opacity-50 bg-error-400 w-1/4 h-1/2 rounded-lg"></div>
+    <div class="border dark:border-white border-black dark:border-opacity-50 border-opacity-50 bg-error-400  w-1/4 h-3/4 rounded-lg"></div>
+    <div class="border dark:border-white border-black dark:border-opacity-50 border-opacity-50 bg-error-400 w-1/4 h-full rounded-lg"></div>
 </div>
-
