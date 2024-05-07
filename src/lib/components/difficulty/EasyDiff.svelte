51faea52fc09bd73dcfea759021c16dd2dcdc4c0
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-05-07 16:04:32 +0200

    FEAT: adding basic setup for difficulty

---
+++ src/lib/components/difficulty/EasyDiff.svelte
@@ -0,0 +1,6 @@
+<div class="flex items-end justify-start space-x-0.5 w-5 h-5 ">
+    <div class="bg-green-500 w-1/4 h-1/2"></div>
+    <div class="border border-black border-opacity-50  w-1/4 h-3/4"></div>
+    <div class="border border-black border-opacity-50  w-1/4 h-full"></div>
+</div>
+
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,10 @@
 
 // Components
 import Meta from "$lib/components/Meta.svelte";
-import Tag from "$lib/components/Tag.svelte";
-
-export {Meta, Tag}
+import Header from "$lib/components/Header.svelte";
+import Grid from "$lib/components/Grid.svelte";
+import EasyDiff  from "$lib/components/difficulty/EasyDiff.svelte";
+import HardDiff from "$lib/components/difficulty/HardDiff.svelte";
+import MedDiff from "$lib/components/difficulty/MedDiff.svelte";
+import DifficultySelection from "$lib/components/difficulty/DifficultySelection.svelte";
+export {Meta, Header, Grid, EasyDiff, MedDiff, HardDiff, DifficultySelection}
