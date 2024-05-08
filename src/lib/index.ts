c6927885e80a44f60defe61944cdbf3a97cad356
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-05-08 09:48:22 +0200

    FEAT: merging different bars into a single changing component (change due to comments)

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,14 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // Components
+// place files you want to import through the `$lib` alias in this folder.
+
+// Components
 import Meta from "$lib/components/Meta.svelte";
 import Header from "$lib/components/Header.svelte";
 import Grid from "$lib/components/Grid.svelte";
-import EasyDiff  from "$lib/components/difficulty/EasyDiff.svelte";
-import HardDiff from "$lib/components/difficulty/HardDiff.svelte";
-import MedDiff from "$lib/components/difficulty/MedDiff.svelte";
 import DifficultySelection from "$lib/components/difficulty/DifficultySelection.svelte";
-export {Meta, Header, Grid, EasyDiff, MedDiff, HardDiff, DifficultySelection}
+import Tag from "$lib/components/Tag.svelte";
+import DiffBar from "$lib/components/difficulty/DiffBar.svelte";
+
+export {Tag,Meta, Header, Grid, DifficultySelection, DiffBar}
