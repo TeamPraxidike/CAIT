e6ba74a741bed7cb5cefa8f905f4ef3b133b40af
“Vasko <V.V.Guenov@student.tudelft.nl>
2024-05-09 12:56:40 +0200

    feat: fully responsive application theory bar

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,16 +1,10 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // Components
-// place files you want to import through the `$lib` alias in this folder.
-
-// Components
 import Meta from "$lib/components/Meta.svelte";
 import Header from "$lib/components/Header.svelte";
 import Grid from "$lib/components/Grid.svelte";
-import SearchBar from "$lib/components/SearchBar.svelte";
-import UserProp from "$lib/components/UserProp.svelte";
-import DifficultySelection from "$lib/components/difficulty/DifficultySelection.svelte";
-import Tag from "$lib/components/Tag.svelte";
-import DiffBar from "$lib/components/difficulty/DiffBar.svelte";
+import TheoryAppBar from "$lib/components/TheoryAppBar.svelte";
+import value from "$lib/components/TheoryAppBar.svelte";
 
-export {Tag,Meta, Header, Grid, DifficultySelection, DiffBar, SearchBar, UserProp}
+export {Meta, Header, Grid, TheoryAppBar, value}
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,13 @@
-<h1>ABOUT</h1>
+<script lang="ts">
+    import { TheoryAppBar} from "$lib";
+
+    let finalValue:number = 0.75;
+
+
+</script>
+
+    <h1>ABOUT</h1>
+<div class = "flex">
+    <div><TheoryAppBar bind:value={finalValue} editable={true}></TheoryAppBar></div>
+        <p>Final value: {finalValue}</p>
+</div>
