3ed7dbb7ec237580d30dfae01ee0b4198b5ab5fa
“Vasko <V.V.Guenov@student.tudelft.nl>
2024-05-10 22:39:19 +0200

    fix: remove about page cluttter and  cait_theme.ts clutter

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,22 +1,9 @@
 <script lang="ts">
     import {PublicationCard} from "$lib";
-    let tags = ["Ann","Ann","hdhd","Matrix"]
-    let tags1 = ["K-nn","ML","Ann","CNN"]
-    let tags2 = ["Ann","Ann","hdhd","Stnoiebw njnjnb "]
-    let tags3 = ["The most decriptive tag possible that could exist ever"]
+
 
 
 </script>
 
 
-<PublicationCard name="Very very very very very very very very very very very looooooooooooooooooooooooooooooooooooooooo" liked="{true}" saved="{false}" tags={tags} numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" used="{7}" tags={tags1} numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" tags={tags} numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" tags={tags2} numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" tags={tags3} numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" tags={tags3} numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" tags={tags3} numMaterials="{3}"/>
-
-<PublicationCard liked="{true}" saved="{false}" numMaterials="{3}"/>
-<PublicationCard liked="{true}" saved="{false}" numMaterials="{3}"/>
 
