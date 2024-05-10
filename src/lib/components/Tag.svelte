9adaccc1f703c6e47494aa3aa6e7693df1490805
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-10 23:11:37 +0200

    test: test tag component

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -4,8 +4,11 @@
 
 
 
-<body>
+
+
+
+{#if tagText !== ""}
     <div class="bg-primary-50 inline-flex rounded-lg py-1 px-2">
         <p class="text-surface-800">{tagText}</p>
     </div>
-</body>
+{/if}
--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,2 +1,17 @@
 /* THIS LINE SETS UP EXPECT MATCHERS FOR THE SVELTE TESTING LIBRARY */ /* DO NOT REMOVE THIS LINE */
 import '@testing-library/jest-dom/vitest'
+import { vi } from 'vitest'
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: vi.fn(), // deprecated
+        removeListener: vi.fn(), // deprecated
+        addEventListener: vi.fn(),
+        removeEventListener: vi.fn(),
+        dispatchEvent: vi.fn(),
+    })),
+})
