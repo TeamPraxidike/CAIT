556c517557cfbe94d2db15c83d04947524ac4c17
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-10 22:42:59 +0200

    fix: responsibility on the profile page

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,9 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // Components
+// place files you want to import through the `$lib` alias in this folder.
+
+// Components
 import Meta from "$lib/components/Meta.svelte";
 import Header from "$lib/components/Header.svelte";
 import Grid from "$lib/components/Grid.svelte";
@@ -9,8 +12,9 @@ import UserProp from "$lib/components/UserProp.svelte";
 import DifficultySelection from "$lib/components/difficulty/DifficultySelection.svelte";
 import Tag from "$lib/components/Tag.svelte";
 import DiffBar from "$lib/components/difficulty/DiffBar.svelte";
+import UserProfileBar from "$lib/components/UserProfileBar.svelte";
 import TheoryAppBar from "$lib/components/TheoryAppBar.svelte";
 import PublicationCard from "$lib/components/PublicationCard.svelte";
 import Comment from "$lib/components/Comment.svelte";
 
-export {Tag,Meta, Header, Grid, DifficultySelection, DiffBar, TheoryAppBar, PublicationCard, SearchBar, UserProp, Comment}
+export {Tag,Meta, Header, Grid, DifficultySelection, DiffBar, UserProfileBar, TheoryAppBar, PublicationCard, SearchBar, UserProp, Comment}
