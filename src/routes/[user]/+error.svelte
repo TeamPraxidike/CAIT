ef8cf14a985a0f992d7024d5606d9202a3eecf84
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-10 21:43:57 +0200

    feat: basic querying to back-end and simple user ! BREAKING: overwritten old user/id

---
+++ src/routes/[user]/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import {Meta} from "$lib";
+    import {page} from "$app/stores";
+
+    let {title, description, type} = {
+        title: $page.status.toString(),
+        description: $page.error ?
+            $page.error.message :
+            "An error occurred while loading the page. Please try again later.",
+        type: "site"
+    }
+
+
+</script>
+
+<Meta {title} {description} {type} />
+
+<p>{$page.status}</p>
+<p>{$page.error?.message}</p>
