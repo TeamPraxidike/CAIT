4b02425223215fa179f3ec3e4dfb4790aada0642
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-05-10 15:42:04 +0200

    FEAT: better element structure

--- src/lib/components/UserPreview/UserHomePreview.svelte
+++ src/lib/components/UserPreview/UserHomePreview.svelte
@@ -1,30 +1,27 @@
 <script lang="ts">
-
-    export let userId:number
-    import {createEventDispatcher} from "svelte";
-
-    export let rep:number
-    export let posts:number
-    export let name:string
-    export let userPhotoUrl:string
-    const dispatch = createEventDispatcher()
-    export const enterUserProfile = (user: { userId: number }) => {
-        dispatch("UserProfileEntry", {value:user.userId})
-    }
+    export let userId:number|null;
+    export let rep:number;
+    export let posts:number;
+    export let name:string;
+    export let userPhotoUrl:string;
+    let link:string;
+    if(userId===null)
+        link="/user"
+    else
+        link = `/user/${name}/${userId}`
 </script>
-<button on:click={() => enterUserProfile({userId})}>
-    <div class="w-48 h-20 text-surface-800 dark:text-surface-50 overflow-hidden card dark:bg-surface-700 pl-2 pr-2 card-hover bg-surface-50 hover:cursor-pointer rounded-lg hover:shadow-lg shadow hover:ring-1 hover:ring-primary-600 hover:ring-opacity-20r">
-        <div class="flex space-x-3 items-center">
-            <div class="w-12 h-12 {userPhotoUrl === '' ? 'bg-surface-500 placeholder-circle' :  ''  }">
+
+<a href={link} class="col-span-2 h-20 text-surface-800 dark:text-surface-50 overflow-hidden card dark:bg-surface-700 pl-2 pr-2 card-hover bg-surface-50 hover:cursor-pointer rounded-lg hover:shadow-lg shadow hover:ring-1 hover:ring-primary-600 hover:ring-opacity-20r">
+    <div class="flex space-x-3 items-center">
+        <div class="w-12 h-12 {userPhotoUrl === '' ? 'bg-surface-500 placeholder-circle' :  ''  }">
+        </div>
+        <div class="flex flex-col items-start w-full">
+            <div class="max-w-full">
+                <span class="dark:text-surface-50 max-w-full hover:overflow-visible truncate">{name}</span>
+                <hr class="dark:bg-surface-50 bg-surface-300"/>
             </div>
-            <div class="flex flex-col items-start w-full">
-                <div class="max-w-full">
-                    <span class="dark:text-surface-50 max-w-full hover:overflow-visible truncate">{name}</span>
-                    <hr class="dark:bg-surface-50 bg-surface-300"/>
-                </div>
-                <span>Rep: {rep}</span>
-                <span>Posts: {posts}</span>
-            </div>
+            <span>Rep: {rep}</span>
+            <span>Posts: {posts}</span>
         </div>
     </div>
-</button>
+</a>
--- src/lib/components/UserPreview/UserMaterialView.svelte
+++ src/lib/components/UserPreview/UserMaterialView.svelte
@@ -1,26 +1,24 @@
 <script lang="ts">
-    import {createEventDispatcher} from "svelte";
-
-    export let userId:number
+    export let userId:number|null
     export let name="Ivan"
     export let userPhoto = ""
     export let role:"Maintainer"|"Publisher"
-    const dispatch = createEventDispatcher()
-    export const enterUserProfile = (user: { userId: number }) => {
-        dispatch("UserProfileEntry", {value:user.userId})
-    }
+    let link:string;
+    if(userId===null)
+        link="/user"
+    else
+        link = `/user/${name}/${userId}`
 </script>
-<button on:click={() => enterUserProfile({userId})}>
-        <div style="width:fit-content" class="overflow-hidden card dark:bg-surface-700 p-2 card-hover bg-surface-50 hover:cursor-pointer rounded-lg hover:shadow-lg shadow hover:ring-1 hover:ring-primary-600 hover:ring-opacity-20r">
-            <div style="width:fit-content" class="flex flex-col flex-1 items-center justify-center space-y-1">
-                <div class="w-12 h-12 {userPhoto === '' ? 'bg-surface-500 placeholder-circle' :  ''  }"/>
-                <div>
-                    <div>{name}</div>
-                    <hr class="bg-surface-300"/>
-                </div>
-                <div>{role}</div>
-            </div>
+
+<a href={link}  class="col-span-1 md:h-28 overflow-hidden card dark:bg-surface-700 p-2 card-hover bg-surface-50 hover:cursor-pointer rounded-lg hover:shadow-lg shadow hover:ring-1 hover:ring-primary-600 hover:ring-opacity-20r">
+    <div class="flex flex-col flex-1 items-center justify-center space-y-1">
+        <div class="w-12 h-12 {userPhoto === '' ? 'bg-surface-500 placeholder-circle' :  ''  }"/>
+        <div>
+            <div>{name}</div>
+            <hr class="bg-surface-300"/>
         </div>
-</button>
+        <div class="text-sm md:text-md ">{role}</div>
+    </div>
+</a>
 
 
