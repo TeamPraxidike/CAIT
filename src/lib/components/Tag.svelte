f5d80b89a04e7dddfd85e7cb66049aa3ae7767fc
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-10 23:11:37 +0200

    test: test tag component

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-    export let tagText:string;
-    export let width:number;
+    export let tagText:string
 </script>
 
 
 
-<div bind:clientWidth={width} class="bg-primary-50 bg-opacity-50 dark:bg-primary-400 dark:bg-opacity-25 inline-flex rounded-lg py-1 px-2  border border-surface-200 dark:border-surface-600 whitespace-nowrap" >
-    <p class="dark:text-surface-100 text-xs text-primary-500 text-opacity-50 dark:text-primary-300 ">{tagText}</p>
-</div>
+
+
+
+{#if tagText !== ""}
+    <div class="bg-primary-50 inline-flex rounded-lg py-1 px-2">
+        <p class="text-surface-800">{tagText}</p>
+    </div>
+{/if}
---
+++ tests/unit/components/reusable.test.ts
@@ -0,0 +1,28 @@
+import {describe, it, expect, beforeEach} from "vitest";
+import {Tag} from "$lib"
+
+describe("Tag Component", () => {
+
+    let host: HTMLDivElement;
+
+    beforeEach(() => {
+        host = document.createElement("div")
+        document.body.appendChild(host);
+    });
+
+    it("should render tag", () => {
+        const instance = new Tag({ target: host, props: { tagText: "example" } });
+        expect(instance).toBeTruthy();
+        expect(host.innerHTML).toContain("example");
+        expect(host.innerHTML).toContain("<!--<Tag>-->");
+    })
+
+    it("should not display tag if tagText is empty", () => {
+        const instance = new Tag({ target: host, props: { tagText: "" } });
+        expect(instance).toBeTruthy();
+
+        // should not render anything besides the comment that the tag is there
+        expect(host.innerHTML).toContain("<!--<Tag>-->");
+        expect(host.innerHTML).not.toContain("</p>");
+    });
+})
