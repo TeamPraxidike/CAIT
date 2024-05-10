4a1274644a0f32c60a70f9f2590bc3d46e66fee3
“Vasko <V.V.Guenov@student.tudelft.nl>
2024-05-10 23:08:45 +0200

    fix: Fixed the TheoryAppBar.svelteto include comments and only on:mouse events instead of documents and windows

--- src/lib/components/TheoryAppBar.svelte
+++ src/lib/components/TheoryAppBar.svelte
@@ -1,67 +1,45 @@
 <script lang="ts">
     import Icon from "@iconify/svelte";
-    //import { writable } from 'svelte/store';
-    import {onMount} from "svelte";
 
     export let value: number = 0.5;
-    export let editable: boolean = false;
+    export let editable: boolean = true;
     let divElement:HTMLDivElement;
     let slider: DOMRect
+    let isDragging = false;
+
+    //Clamp the value between 1 and 100
     const clamp = (x: number): number => Math.min(1, Math.max(0, x));
-
-
-
-
-
-    onMount(() => {
-        let isDragging = false;
-        slider = divElement.getBoundingClientRect();
-
-
-        const handleOnMove = (e: MouseEvent) => {
-
-            if (!isDragging || !editable) {
-                return;
-            }
-            value = Number(clamp(slider ? (e.clientX - slider.left) / slider.width : 0).toFixed(2));
+    const handleOnMove = (e: MouseEvent) => {
+        //If we are not dragging or the element is simply not editable(e.g. view page) we ignore the movement
+        if (!isDragging || !editable) {
+            return;
         }
 
-        const handleOnMoveTouch = (e: TouchEvent) => {
+        // check the width and assign the percentage value
+        value = Number(clamp(slider ? (e.clientX - slider.left) / slider.width : 0).toFixed(2));
+    }
 
-            if (!isDragging || !editable) {
-                return;
-            }
-            value = Number(clamp(slider ? (e.touches[0].clientX - slider.left) / slider.width : 0).toFixed(2));
+    const handleOnMoveTouch = (e: TouchEvent) => {
+
+        if (!isDragging || !editable) {
+            return;
         }
-
-        document.onmousedown = e => {
-            slider = divElement.getBoundingClientRect();
-                if (e.clientX >= slider.left && e.clientX <= slider.right && e.clientY >= slider.top && e.clientY <= slider.bottom) {
-                    isDragging = true;
-                    handleOnMove(e);
-                }
-            }
-        document.ontouchstart = e => {
-            slider = divElement.getBoundingClientRect();
-
-            if (e.touches[0].clientX >= slider.left && e.touches[0].clientX <= slider.right && e.touches[0].clientY >= slider.top && e.touches[0].clientY <= slider.bottom) {
-                isDragging = true;
-                handleOnMoveTouch(e);
-            }
-        }
+        value = Number(clamp(slider ? (e.touches[0].clientX - slider.left) / slider.width : 0).toFixed(2));
+    }
 
 
-        document.onmousemove = e => handleOnMove(e);
-        document.ontouchmove = e => handleOnMoveTouch(e);
+    // Start the dragging process and update the slider property
+    const start = () => {
+        //update the slider element in case the window has been resized
+        slider = divElement.getBoundingClientRect()
 
+        //initiate the dragging sequence by setting the isDragging to true
+        isDragging=true
+    }
 
-        document.onmouseup = () => {
-            isDragging = false;
-        }
-        document.ontouchend= () => {
-            isDragging = false;
-        }
-    });
+    //Finish the dragging process
+    const finsih = () => {isDragging=false}
+
 
 </script>
 <div class = "flex items-center justify-center items-center ">
@@ -71,7 +49,7 @@
         </div>
         <p class = "text-surface-50 self-center hidden md:block md:text-xs">Theory</p>
     </div>
-    <div bind:this={divElement} >
+    <div tabindex="0" role="button" bind:this={divElement} on:mousemove={handleOnMove} on:touchmove={handleOnMoveTouch} on:mousedown={start} on:mouseup={finsih} on:touchstart={start} on:touchend={finsih}>
         <div class = "relative flex items-center items-center w-24 md:w-36 xl:w-44 h-4 md:h-6 xl:h-8 border-y border-surface-600">
             <div class = "bg-primary-500 h-4 md:h-6 xl:h-8 flex items-center border-y  border-surface-600 " style="width: {value*100}%"/>
             <div class = "bg-surface-50 flex-grow h-4 md:h-6 xl:h-8 justify-self-end flex  items-center border-y border-l border-surface-600"/>
