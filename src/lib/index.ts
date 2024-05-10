ea91ab25899c008814379720b40a2eab818eb1a2
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-05-10 18:42:37 +0200

    FEAT: combine into one file and change based on a single export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -8,12 +8,9 @@ import Meta from "$lib/components/Meta.svelte";
 import Header from "$lib/components/Header.svelte";
 import Grid from "$lib/components/Grid.svelte";
 import SearchBar from "$lib/components/SearchBar.svelte";
-import UserHomePreview from "$lib/components/UserPreview/UserHomePreview.svelte";
-import UserMaterialView from "$lib/components/UserPreview/UserMaterialView.svelte";
-import UserPublishView from "$lib/components/UserPreview/UserPublishView.svelte";
-import UserSearch from "$lib/components/UserPreview/UserSearch.svelte";
+import UserProp from "$lib/components/UserProp.svelte";
 import DifficultySelection from "$lib/components/difficulty/DifficultySelection.svelte";
 import Tag from "$lib/components/Tag.svelte";
 import DiffBar from "$lib/components/difficulty/DiffBar.svelte";
 
-export {Tag,Meta, Header, Grid, DifficultySelection, DiffBar, SearchBar, UserHomePreview,UserPublishView,UserSearch,UserMaterialView}
+export {Tag,Meta, Header, Grid, DifficultySelection, DiffBar, SearchBar, UserProp}
