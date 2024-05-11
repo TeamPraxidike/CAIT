463db4242f7131f76c67bfb70c08a277a85fd29c
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-11 17:57:51 +0200

    feat: auth store

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import {Grid} from "$lib";
-    import { LightSwitch } from '@skeletonlabs/skeleton';
+    import {authStore, Grid} from "$lib";
+    import {LightSwitch, type PopupSettings, popup} from '@skeletonlabs/skeleton';
     import Icon from "@iconify/svelte";
     import { slide } from 'svelte/transition';
     import { quartOut } from 'svelte/easing';
@@ -9,47 +9,49 @@
         text: string;
         link: string;
     }
-
-    let dropDown: boolean = false;
-    let loggedIn: boolean = false;
-
     const navOptions : NavOption[] = [
         { text: 'About', link: '/about' },
-        { text: 'Browse Publications', link: '/publications' },
-        { text: 'My Publications', link: '/profile/mypublications' },
-        { text: 'Saved Materials', link: '/profile/saved' },
-
-
     ]
 
-    function toggleDropDown() : void{
-        dropDown = !dropDown
-    }
+    const popupHover: PopupSettings = {
+        event: 'click',
+        target: 'popupHover',
+        placement: 'bottom'
+    };
 
+    let dropDown: boolean = false;
+    let loggedIn: boolean;
+    $: loggedIn = $authStore.userid !== null;
+
+    const toggleDropDown = () => dropDown = !dropDown;
+
+    // TODO: THIS WOULD ACTUALLY BE A CALL TO THE AUTH SERVICE
+    const login = () => authStore.setAuth(1, '1234');
+    const logout = () => authStore.clearAuth();
 </script>
 
-<header class="w-screen shadow-lg dark:bg-surface-900  bg-surface-50 border-b border-surface-600 dark:border-surface-300 md:border-none">
+<header class="w-screen shadow-lg dark:bg-surface-800  bg-surface-50 border-b border-surface-600 dark:border-surface-300 md:border-none">
     <Grid>
-        <div class = "col-start-1">
+        <a href="/" class = "col-start-1">
             <enhanced:img class="h-16 w-16 md:hidden" src="/static/favicon.png" alt="CAIT Logo"/>
             <enhanced:img class="h-16 w-16 hidden md:block" src="/static/Logo.webp" alt="CAIT Logo"/>
-        </div>
+        </a>
 
-        <div class="hidden col-start-2 col-span-7 gap-2 lg:gap-4 items-center justify-center md:flex">
+        <div class="hidden col-start-2 col-span-7 gap-4 lg:gap-8 items-center md:flex">
             {#each navOptions as opt}
-                <a class=" group transition text-surface-800 dark:text-surface-50 h-full flex items-center justify-center md:px-2 xl:px-4" href={opt.link} >
-                    <span class="bg-left-bottom bg-gradient-to-r from-primary-500 to-primary-500 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-300 ease-in flex items-center justify-center">
+                <a class="group transition text-surface-800 dark:text-surface-50 h-full flex items-center" href={opt.link} >
+                    <span class="bg-left-bottom bg-gradient-to-r from-primary-400 to-primary-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-300 ease-in">
                         {opt.text}
                     </span>
                 </a>
             {/each}
         </div>
 
-        <div class="hidden md:flex col-end-13 col-span-2 md:gap-2 xl:gap-4 items-center justify-self-end">
+        <div class="hidden md:flex col-start-11 col-span-2 md:gap-2 xl:gap-4 items-center justify-self-end">
                 {#if loggedIn}
-                    <button class="hidden md:block btn rounded-lg md:py-1 lg:py-1.5 md:px-2 lg:px-3  bg-primary-600 text-surface-50 hover:opacity-60 transition duration-400">Publish</button>
+                    <button class="hidden md:block btn rounded-lg md:py-1 lg:py-1.5 md:px-2 lg:px-3 bg-primary-600 text-surface-50 hover:opacity-60 transition duration-400">Publish</button>
                 {:else}
-                    <button class="hidden md:block btn rounded-lg md:py-1 lg:py-1.5 md:px-2 lg:px-3  bg-primary-600 text-surface-50 hover:opacity-60 transition duration-400">Sign In</button>
+                    <button on:click={login} class="hidden md:block btn rounded-lg md:py-1 lg:py-1.5 md:px-2 lg:px-3 bg-primary-600 text-surface-50 hover:opacity-60 transition duration-400">Sign In</button>
                 {/if}
             <div class="border-l border-surface-300 h-8"/>
             <div>
@@ -57,11 +59,17 @@
             </div>
 
             {#if loggedIn}
-            <div class="border-l border-surface-300 h-8"/>
-            <div>
-                <Icon icon="gg:profile" className = "text-surface-600 justify-self-end" width="32" height="32"/>
-            </div>
-                {/if}
+                <div class="border-l border-surface-300 h-8"/>
+                <div use:popup={popupHover} class="cursor-pointer [&>*]:pointer-events-none">
+                    <enhanced:img class="h-8 w-8 rounded-full" src="/static/fdr.jpg" alt="Profile Picture"/>
+                </div>
+                <div class="card p-4 bg-surface-50 border border-surface-300
+                            dark:text-surface-50" data-popup="popupHover">
+                    <a href="/{$authStore.userid}">Profile</a>
+                    <div class="text-sm">Hello, {$authStore.userid}</div>
+                    <button on:click={logout} class="variant-soft-surface rounded-lg p-1">Log out</button>
+                </div>
+            {/if}
         </div>
 
         <button  class = "col-end-5 self-center justify-self-end w-12 h-8 md:hidden" on:click={toggleDropDown}>
@@ -87,7 +95,7 @@
                             <LightSwitch />
                         </div>
 
-                        <button class="btn rounded-lg variant-ghost-primary">Sign In</button>
+                        <button on:click={login} class="btn rounded-lg variant-ghost-primary">Sign In</button>
                     </div>
                 </div>
             </div>
