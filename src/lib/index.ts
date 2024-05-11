bc6d94b5ba011987346ebeab533db10615ebac3e
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-11 19:33:23 +0200

    chore: organize components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,17 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 // Components
-import Meta from "$lib/components/Meta.svelte";
-import Header from "$lib/components/Header.svelte";
-import Grid from "$lib/components/Grid.svelte";
+import Meta from "$lib/components/page/Meta.svelte";
+import Header from "$lib/components/page/Header.svelte";
+import Grid from "$lib/components/generic/Grid.svelte";
 import SearchBar from "$lib/components/SearchBar.svelte";
 import UserProp from "$lib/components/UserProp.svelte";
 import DifficultySelection from "$lib/components/difficulty/DifficultySelection.svelte";
-import Tag from "$lib/components/Tag.svelte";
+import Tag from "$lib/components/generic/Tag.svelte";
 import DiffBar from "$lib/components/difficulty/DiffBar.svelte";
-import UserProfileBar from "$lib/components/UserProfileBar.svelte";
-import IconLink from "$lib/components/IconLink.svelte";
-import UserMenu from "$lib/components/UserMenu.svelte";
+import UserProfileBar from "$lib/components/user/UserProfileBar.svelte";
+import IconLink from "$lib/components/generic/IconLink.svelte";
+import UserMenu from "$lib/components/user/UserMenu.svelte";
 
 // Stores
 import {authStore} from "$lib/stores/auth";
