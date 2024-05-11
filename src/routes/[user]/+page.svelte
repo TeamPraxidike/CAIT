4644cdeed6fe4c1313a0abe5a34cb27bbdd9646f
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-11 19:29:19 +0200

    refactor: cleanup header

--- src/routes/[user]/+page.svelte
+++ src/routes/[user]/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
     import {Meta, UserProfileBar} from "$lib";
-    import type { PageServerData } from './$types';
+    import type { LayoutData } from './$types';
     import type {User} from "@prisma/client";
 
     /* This is the data that was returned from the server */
-    export let data:PageServerData;
+    export let data:LayoutData;
 
     let user:User = data.user;
 </script>
@@ -13,7 +13,7 @@
 
 <UserProfileBar {user} />
 
-<h3 class="text-xl mt-8 text-surface-900 col-span-4 text-center
+<h3 class="text-xl mt-8 text-surface-900 col-span-4 text-center dark:text-surface-50
            md:col-span-8
            lg:col-span-12
            xl:col-span-8">
@@ -22,7 +22,7 @@
 
 <!-- TODO: @vguenov add your components here! Just plop them and the grid will take care of it -->
 
-<h3 class="text-xl mt-8 text-surface-900 col-span-4 text-center
+<h3 class="text-xl mt-8 text-surface-900 col-span-4 text-center dark:text-surface-50
            md:col-span-8
            lg:col-span-12
            xl:col-span-8">
