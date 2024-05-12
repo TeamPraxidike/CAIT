67796d200d649099f8afdb6e75f1087a26c9c57b
k.panayotov@student.tudelft.nl <eccyboo@protonmail.com>
2024-05-13 00:57:43 +0200

    fix: tag fix

--- src/lib/components/generic/Tag.svelte
+++ src/lib/components/generic/Tag.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
     export let tagText:string
+    export let width:number;
 </script>
 
-
-
-
-
-
 {#if tagText !== ""}
-    <div class="bg-primary-50 inline-flex rounded-lg py-1 px-2">
+    <div bind:clientWidth={width} class="bg-primary-50 inline-flex rounded-lg py-1 px-2">
         <p class="text-surface-800">{tagText}</p>
     </div>
 {/if}
