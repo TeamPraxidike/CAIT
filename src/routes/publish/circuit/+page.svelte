c06d44aaf125568af654c70797865d711cf9f361
Martin Damyanov <m.damyanov@student.tudelft.nl>
2024-05-31 16:11:55 +0200

    fix: LO and PK components

--- src/routes/publish/circuit/+page.svelte
+++ src/routes/publish/circuit/+page.svelte
@@ -15,6 +15,7 @@
 	import { goto } from '$app/navigation';
 	import type { NodeDiffActions } from '$lib/database';
 	import Icon from '@iconify/svelte';
+	import MetadataLOandPK from "$lib/components/MetadataLOandPK.svelte";
 
 	export let data: PageServerData;
 
@@ -253,96 +254,29 @@
 				formData.append('coverPic', JSON.stringify(circuitCoverPic));
       }}>
 	<Stepper on:next={onNextHandler} buttonCompleteType="submit">
-		<Step >
-			<svelte:fragment slot="header">Create the circuit</svelte:fragment>
-			<Circuit bind:this={circuitRef} publishing="{true}"/>
-		</Step>
-		<Step locked="{locks[0]}">
-			<svelte:fragment slot="header">Give your publication a title</svelte:fragment>
-			<div class="flex flex-col gap-5 col-span-full">
-				<div class="w-full space-y-1">
-					<label for="circuitTitle" >Title<span class="text-error-300">*</span></label>
-					<input bind:value={title} id="circuitTitle" class="rounded-lg w-full dark:bg-surface-800 bg-surface-50 text-surface-700 dark:text-surface-400 focus:ring-primary-500" placeholder="Enter title" required/>
-				</div>
-				<div class="w-full space-y-1">
-					<label for="circuitDescription">Description<span class="text-error-300">*</span></label>
-					<textarea bind:value={description} rows="5" id="circuitDescription" class="rounded-lg w-full dark:bg-surface-800 bg-surface-50 text-surface-700 dark:text-surface-400 focus:ring-primary-500" placeholder="Explain your circuit" required />
-				</div>
-			</div>
+<!--		<Step >-->
+<!--			<svelte:fragment slot="header">Create the circuit</svelte:fragment>-->
+<!--			<Circuit bind:this={circuitRef} publishing="{true}"/>-->
+<!--		</Step>-->
+<!--		<Step locked="{locks[0]}">-->
+<!--			<svelte:fragment slot="header">Give your publication a title</svelte:fragment>-->
+<!--			<div class="flex flex-col gap-5 col-span-full">-->
+<!--				<div class="w-full space-y-1">-->
+<!--					<label for="circuitTitle" >Title<span class="text-error-300">*</span></label>-->
+<!--					<input bind:value={title} id="circuitTitle" class="rounded-lg w-full dark:bg-surface-800 bg-surface-50 text-surface-700 dark:text-surface-400 focus:ring-primary-500" placeholder="Enter title" required/>-->
+<!--				</div>-->
+<!--				<div class="w-full space-y-1">-->
+<!--					<label for="circuitDescription">Description<span class="text-error-300">*</span></label>-->
+<!--					<textarea bind:value={description} rows="5" id="circuitDescription" class="rounded-lg w-full dark:bg-surface-800 bg-surface-50 text-surface-700 dark:text-surface-400 focus:ring-primary-500" placeholder="Explain your circuit" required />-->
+<!--				</div>-->
+<!--			</div>-->
 
-		</Step>
+<!--		</Step>-->
 		<Step locked="{locks[1]}">
 			<svelte:fragment slot="header">Additional Metadata</svelte:fragment>
 			<div class="flex flex-col justify-between gap-3 col-span-full">
-					<div class="flex flex-col space-y-1 w-full">
-						<label for="learningObjective" >Learning Objectives<span class="text-error-300">*</span>:</label>
-						<div class="w-full">
-							<div class="w-full flex justify-between gap-2">
-								<input on:keydown={handleLOPress}  bind:this={loInput} id="learningObjective" type="text" placeholder="Enter learning objective" class="rounded-lg dark:bg-surface-800 bg-surface-50 text-surface-700 dark:text-surface-400 w-full focus:ring-primary-500 focus:ring-1 "/>
-								<button on:click={()=>{ if(LOs.includes(loInput.value)) { triggerRepeatInput("Learning Objective",loInput.value)} else { if(loInput.value!=='') {LOs = [...LOs, loInput.value]; loInput.value = "";}}}} type="button" name="add_LO" inputmode="decimal"
-												class="text-center">
-									<Icon icon="mdi:plus-circle" width="32" height="32"  class="bg-surface-0 text-surface-800 hover:text-surface-600" />
-								</button>
-							</div>
 
-							<div class="overflow-y-auto max-h-56 mt-1 space-y-1 flex flex-col">
-								{#each LOs as LO, index}
-									{#if editingLO && index === editingIndexLO}
-										<input class="bg-surface-200 focus:ring-0 ring-0 rounded-lg max-h-full w-full text-md" bind:value={editingLOText} on:keypress={handleLOEdit}/>
-									{:else }
-										<div role="table" tabindex="-1" class="p-2 flex rounded-lg justify-between bg-surface-200 items-center w-1/2 text-wrap" on:mouseenter={()=>{displayButtonLO=true; hoverIndexLO=index}} on:mouseleave={()=>{displayButtonLO=false;}}>
-											<span class="relative right-1 text-base p-1 rounded-md">{LO}</span>
-											{#if !editingLO && displayButtonLO && hoverIndexLO ===index}
-												<div class=" items-end flex gap-2" >
-													<button type="button" class="self-center rounded-lg" on:click={() => {editingLO=true; editingIndexLO=index; editingLOText=LO;}}>
-														<Icon icon="mdi:pencil" width="20" height="20"  class="text-surface-700 ml-auto hover:text-opacity-85" />
-													</button>
-													<button type="button" class=" self-center rounded-lg" on:click={() => {LOs = LOs.filter(x=>x!==LO)}}>
-														<Icon icon="mdi:trash-can" width="20" height="20" class="text-error-300 ml-auto hover:text-opacity-85" />
-													</button>
-												</div>
-											{/if}
-										</div>
-									{/if}
-								{/each}
-							</div>
-						</div>
-					</div>
-
-					<div class="w-full">
-						<label for="priorKnowledge" class="mb-1" >Prior Knowledge:</label>
-
-						<div class="flex w-full justify-between gap-2">
-							<input   bind:this={priorInput} on:keydown={handlePriorPress} id="priorKnowledge" type="text" placeholder="Enter needed concept" class="rounded-lg dark:bg-surface-800 bg-surface-50 text-surface-700 dark:text-surface-400 w-full focus:ring-primary-500"/>
-							<button on:click={()=>{if(priorKnowledge.includes(priorInput.value)) {triggerRepeatInput("Prior Knowledge",priorInput.value)}  if(priorInput.value!=='') {priorKnowledge = [...priorKnowledge, priorInput.value]; priorInput.value = "";}}} type="button" name="add_prior" inputmode="decimal"
-											class="text-center">
-								<Icon icon="mdi:plus-circle" width="32" height="32"  class="bg-surface-0 text-surface-800 hover:text-surface-600" />
-							</button>
-						</div>
-
-
-						<div class="overflow-y-auto max-h-56 mt-1 space-y-1 flex flex-col">
-							{#each priorKnowledge as pk, index}
-								{#if editingPK && index === editingIndexPK}
-									<input class="bg-surface-200 focus:ring-0 ring-0 rounded-lg max-h-full w-full text-md" bind:value={editingPKText} on:keypress={handlePKEdit}/>
-								{:else }
-									<div role="table" tabindex="-1" class="p-2 flex rounded-lg justify-between bg-surface-200 items-center w-1/2 text-wrap" on:mouseenter={()=>{displayButton=true; hoverIndexPK=index}} on:mouseleave={()=>{displayButton=false;}}>
-										<p class="relative right-1 text-base p-1 rounded-md">{pk}</p>
-										{#if !editingPK && displayButton && hoverIndexPK === index }
-											<div class=" items-end flex gap-2" >
-												<button type="button" class="self-center rounded-lg" on:click={() => {editingPK=true; editingIndexPK=index; editingPKText=pk;}}>
-													<Icon icon="mdi:pencil" width="20" height="20"  class="text-surface-700 ml-auto hover:text-opacity-85" />
-												</button>
-												<button type="button" class=" self-center rounded-lg" on:click={() => {priorKnowledge = priorKnowledge.filter(x=>x!==pk)}}>
-													<Icon icon="mdi:trash-can" width="16" height="16" class="text-error-300 ml-auto hover:text-opacity-85" />
-												</button>
-											</div>
-										{/if}
-									</div>
-								{/if}
-							{/each}
-						</div>
-					</div>
+				<MetadataLOandPK />
 
 				<div class="flex flex-col w-full">
 					<div class="flex flex-col gap-2 w-full">
