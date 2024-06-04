f2fcd96cbeef15220d3890635c53fdb06b9d5613
Martin Damyanov <m.damyanov@student.tudelft.nl>
2024-06-04 22:07:54 +0200

    fix: navigation between saved and posts

--- src/routes/[user]/+page.svelte
+++ src/routes/[user]/+page.svelte
@@ -4,6 +4,7 @@
     import { type Publication, PublicationType, type Tag, type User} from '@prisma/client';
     import type {FetchedFileItem} from '$lib/database';
     import { page } from '$app/stores';
+    import { TabGroup, Tab } from '@skeletonlabs/skeleton';
 
     /* This is the data that was returned from the server */
     export let data: LayoutData & PageServerData;
@@ -32,42 +33,69 @@
         usedInCourse: {course: string}[]
     }[] = data.user.posts.filter((x: any) => x.type !== PublicationType.Circuit);
 
-    console.log(posts)
+    let tabSet: number = 0;
 </script>
 
 <Meta title="Profile" description="CAIT" type="site"/>
 
-<UserProfileBar user={user} userPhotoUrl={'data:image;base64,' + profilePic.data}/>
+<div class="col-span-full xl:flex xl:flex-row xl:space-x-5
+                            lg:flex lg:flex-row lg:space-x-5">
 
-<div class="grid col-span-full gap-4 mb-20
-            md:col-span-8 lg:col-span-12 xl:col-span-8">
-    {#if saved.length !== 0}
-        <h3 class="text-xl mt-8 text-surface-900 col-span-6 text-center dark:text-surface-50">
-            Saved Publications
-        </h3>
-
-        {#each data.saved as publication, i}
-            <PublicationCard imgSrc={'data:image;base64,' + savedFileData[i].data} {publication} liked={liked.includes(publication.id)} markAsUsed={true} courses={saved[i].usedInCourse.map(x => x.course)}/>
-        {/each}
-    {/if}
+    <UserProfileBar user={user} userPhotoUrl={'data:image;base64,' + profilePic.data}/>
 
     {#if $page.data.session?.user.id === user.id}
-        <h3 class="text-xl mt-8 text-surface-900 col-span-6 text-center dark:text-surface-50">
-            Your Publications
-        </h3>
-    {:else}
-        <h3 class="text-xl mt-8 text-surface-900 col-span-6 text-center dark:text-surface-50">
-            {user.firstName}'s Publications
-        </h3>
-    {/if}
-
-    {#if postsPublication.length === 0}
-        <p class="col-span-6 text-center">So empty... There are no publications here</p>
-    {:else}
-        {#each postsPublication as publication, i}
-            <PublicationCard imgSrc={'data:image;base64,' + publication.coverPicData} {publication} liked={liked.includes(publication.id)} courses={posts[i].usedInCourse.map(x => x.course)}/>
-        {/each}
-    {/if}
+        <TabGroup justify="justify-center" class="col-span-8 lg:col-span-full">
+            <Tab bind:group={tabSet} name="tab1" value={0}>
+                <p>Saved Publications</p>
+            </Tab>
+            <Tab bind:group={tabSet} name="tab2" value={1}>
+                <p>Your Publications</p>
+            </Tab>
+            <svelte:fragment slot="panel">
+                {#if tabSet === 0}
+                    <div class="grid grid-cols-4 gap-4 mb-20
+               md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
+                        {#if saved.length !== 0}
+                            {#each saved as publication, i}
+                                <div class="col-span-4 md:col-span-2 lg:col-span-2 xl:col-span-2">
+                                    <PublicationCard imgSrc={'data:image;base64,' + savedFileData[i].data} {publication} liked={liked.includes(publication.id)} markAsUsed={true} courses={publication.usedInCourse.map(x => x.course)}/>
+                                </div>
+                            {/each}
+                        {/if}
+                    </div>
+                {:else if tabSet === 1}
+                    <div class="grid grid-cols-4 gap-4 mb-20
+               md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
+                        {#if postsPublication.length === 0}
+                            <p class="col-span-full text-center">So empty... There are no publications here</p>
+                        {:else}
+                            {#each postsPublication as publication, i}
+                                <div class="col-span-4 md:col-span-2 lg:col-span-2 xl:col-span-2">
+                                    <PublicationCard imgSrc={'data:image;base64,' + publication.coverPicData} {publication} liked={liked.includes(publication.id)} courses={posts[i].usedInCourse.map(x => x.course)}/>
+                                </div>
+                            {/each}
+                        {/if}
+                    </div>
+                {/if}
+            </svelte:fragment>
+        </TabGroup>
+        {:else}
+            <div class="grid grid-cols-4 gap-4 mb-20
+                   md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
+                <h3 class="text-xl mt-8 text-surface-900 col-span-full text-center dark:text-surface-50">
+                    {user.firstName}'s Publications
+                </h3>
+                {#if postsPublication.length === 0}
+                    <p class="col-span-full text-center">So empty... There are no publications here</p>
+                {:else}
+                    {#each postsPublication as publication, i}
+                        <div class="col-span-4 md:col-span-2 lg:col-span-2 xl:col-span-2">
+                            <PublicationCard imgSrc={'data:image;base64,' + publication.coverPicData} {publication} liked={liked.includes(publication.id)} courses={posts[i].usedInCourse.map(x => x.course)}/>
+                        </div>
+                    {/each}
+                {/if}
+            </div>
+        {/if}
 </div>
 
 
