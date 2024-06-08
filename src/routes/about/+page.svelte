36f500af245e1aa9e422ab7ed18e9e3083a9a3ae
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-06-08 17:39:00 +0200

    feat: openapi + rep summary

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,3 +5,28 @@
 <Meta title="About" description="CAIT" type="site" />
 
 <h1>About</h1>
+<div class="flex flex-col col-span-full gap-2">
+	<h2 class="font-bold">Reputation System Legend:</h2>
+	<div>
+		<p class="font-semibold">Publication Based:</p>
+			Materials: 30<br>
+			Circuit: 50
+	</div>
+
+	<div>
+		<p class="font-semibold">Communication Based:</p>
+		Comment: 5<br>
+		Comment to own publication: 3<br>
+		Reply to own comment: 3<br>
+		Reply to others: 5<br>
+	</div>
+
+	<div>
+		<p class="font-semibold">Action Based:</p>
+		Somebody upvotes your  publication (not you): 2<br>
+		Somebody upvotes your comment (not you): 1<br>
+		Someone saves your publication (not you): 10<br>
+		Someone indicates they use your publication (not you): 15<br>
+	</div>
+
+</div>
