1cf2f7e70df0f9311b82889075896feb0d864b00
bmarinov <b.k.marinov-1@student.tudelft.nl>
2024-06-11 20:34:11 +0200

    feat: added about page information and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {lorem, Meta, Section} from "$lib";
+    import { Meta, Section} from "$lib";
 </script>
 
 <Meta title="Home" description="CAIT" type="site"/>
@@ -35,6 +35,6 @@
 </div>
 
 <main class="col-span-full flex flex-col gap-10 my-20">
-    <Section title="Circuits" description={lorem} subtitle="Define your learning flow"/>
-    <Section title="Circuits" description={lorem} subtitle="Define your workflow"/>
+    <Section title="Materials" description={'Learn more about the content you can find on this platform'} subtitle="What you can expect to find" img='/materials.svg'/>
+    <Section title="Circuits" description={'Learn more about how to organize publications and give an order to them in a circuit'} subtitle="Define your learning workflow" img='/sectionImages/circuit.png'/>
 </main>
