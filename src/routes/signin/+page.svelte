c529f1e3e50be8497f9b42a347b7d1dbb4b45f4d
Martin Damyanov <m.damyanov@student.tudelft.nl>
2024-06-19 20:08:07 +0200

    feat: center the github button

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -50,7 +50,7 @@
 </script>
 
 <div class="col-span-12 h-[80vh] flex items-center justify-center">
-	<form on:submit|preventDefault={handleSignIn} method="POST" class="flex flex-col w-96 border p-8 rounded-lg shadow text-surface-600 gap-2">
+	<form on:submit|preventDefault={handleSignIn} method="POST" class="flex flex-col w-96 border p-8 rounded-lg shadow text-surface-600 gap-2 justify-center">
 		<h3 class="text-center mt-4 text-surface-700 font-semibold text-xl">Login</h3>
 		<label for="email">
 			Email
@@ -68,12 +68,15 @@
 		</div>
 		<button class="btn rounded-lg mt-4 variant-soft-primary" type="submit">Log in</button>
 		<hr class="my-4 text-surface-200">
-		<SignIn provider="github" signInPage="signin">
-			<button slot="submitButton" class="btn rounded-lg bg-surface-800 text-surface-50">
-				<Icon icon="mdi:github" class="text-2xl mr-2" />
-				Log in with GitHub
-			</button>
-		</SignIn>
+		<div class="self-center">
+			<SignIn provider="github" signInPage="signin">
+				<button slot="submitButton" class="btn rounded-lg bg-surface-800 text-surface-50">
+					<Icon icon="mdi:github" class="text-2xl mr-2" />
+					Log in with GitHub
+				</button>
+			</SignIn>
+		</div>
+
 		<p class="text-center text-sm mt-2">Don't have an account? <a class="anchor text-primary-600" href="/register">Create one</a></p>
 		<div class="{display} text-sm px-2 rounded-lg variant-soft-error text-wrap">
 			<span>{$errorMessage}</span>
