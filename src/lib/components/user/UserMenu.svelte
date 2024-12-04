26b5ecf83e388b2ddc7daf264913f049798c6b4a
Zakrok09 <eccyboo@protonmail.com>
2024-12-04 18:38:42 +0100

    fix: session check in user menu component

--- src/lib/components/user/UserMenu.svelte
+++ src/lib/components/user/UserMenu.svelte
@@ -34,48 +34,49 @@
 
 </script>
 
-{#if device === 'mobile'}
-    <div class="grid grid-cols-2">
-        <a href="/{$page.data.session?.user.id}"  on:click={confirmPublishReset}
-           class="btn justify-start flex gap-2 items-center hover:bg-surface-200 rounded-lg p-1 dark:hover:bg-surface-700 col-span-2">
-            {#if $page.data.session && loggedUser?.profilePicData !== ''}
-                <img class="h-16 w-16 rounded-full object-cover" src={'data:image;base64,' + loggedUser?.profilePicData} alt={loggedUser?.firstName}/>
-            {:else}
-                <div class="h-16 w-16 placeholder-circle object-cover" />
-            {/if}
-            <div class="flex flex-col">
-                <span>{loggedUser?.firstName}</span>
-                <span class="text-sm">Go to profile</span>
-            </div>
-        </a>
-        <IconLink p="p-4" icon="ion:person-sharp" href="/{$page.data.session?.user.id}" link="Profile"/>
-        <IconLink p="p-4" icon="ion:bookmark-sharp" href="/{$page.data.session?.user.id}/saved" link="Saved"/>
-        <IconLink p="p-4" icon="ion:book" href="/{$page.data.session?.user.id}/publications" link="Publications"/>
-        <button on:click={handleSignOut} class="anchor col-start-2">Log out</button>
-
-    </div>
-{:else}
-    <div class="card relative z-20 bg-surface-50 border border-surface-300 p-4 w-64
-                            dark:text-surface-50 dark:border-none">
-        <div class="flex flex-col">
-            <a href="/{$page.data.session?.user?.id}" on:click={confirmPublishReset}
-               class="btn justify-start flex gap-2 items-center hover:bg-surface-200 rounded-lg p-1
-                  dark:hover:bg-surface-700">
-                {#if $page.data.session}
-                    <img class="h-16 w-16 rounded-full object-cover" src={'data:image;base64,' + loggedUser?.profilePicData} alt={loggedUser?.firstName}/>
+{#if loggedUser}
+    {#if device === 'mobile'}
+        <div class="grid grid-cols-2">
+            <a href="/{loggedUser.id}"  on:click={confirmPublishReset}
+               class="btn justify-start flex gap-2 items-center hover:bg-surface-200 rounded-lg p-1 dark:hover:bg-surface-700 col-span-2">
+                {#if loggedUser.profilePicData !== ''}
+                    <img class="h-16 w-16 rounded-full object-cover" src={'data:image;base64,' + loggedUser.profilePicData} alt={loggedUser.firstName}/>
                 {:else}
-                    <div class="h-16 w-16 placeholder-circle" />
+                    <div class="h-16 w-16 placeholder-circle object-cover" />
                 {/if}
                 <div class="flex flex-col">
-                    <span>{loggedUser?.firstName}</span>
+                    <span>{loggedUser.firstName}</span>
                     <span class="text-sm">Go to profile</span>
                 </div>
             </a>
-            <hr class="my-2">
-            <IconLink icon="ion:person-sharp" href="/{$page.data.session?.user.id}" link="Profile"/>
-            <hr class="my-2">
-        </div>
-        <button on:click={handleSignOut} class="anchor">Log out</button>
-    </div>
-{/if}
+            <IconLink p="p-4" icon="ion:person-sharp" href="/{loggedUser.id}" link="Profile"/>
+            <IconLink p="p-4" icon="ion:bookmark-sharp" href="/{loggedUser.id}/saved" link="Saved"/>
+            <IconLink p="p-4" icon="ion:book" href="/{loggedUser.id}/publications" link="Publications"/>
+            <button on:click={handleSignOut} class="anchor col-start-2">Log out</button>
 
+        </div>
+    {:else}
+        <div class="card relative z-20 bg-surface-50 border border-surface-300 p-4 w-64
+                                dark:text-surface-50 dark:border-none">
+            <div class="flex flex-col">
+                <a href="/{loggedUser.id}" on:click={confirmPublishReset}
+                   class="btn justify-start flex gap-2 items-center hover:bg-surface-200 rounded-lg p-1
+                      dark:hover:bg-surface-700">
+                    {#if $page.data.session}
+                        <img class="h-16 w-16 rounded-full object-cover" src={'data:image;base64,' + loggedUser.profilePicData} alt={loggedUser.firstName}/>
+                    {:else}
+                        <div class="h-16 w-16 placeholder-circle" />
+                    {/if}
+                    <div class="flex flex-col">
+                        <span>{loggedUser.firstName}</span>
+                        <span class="text-sm">Go to profile</span>
+                    </div>
+                </a>
+                <hr class="my-2">
+                <IconLink icon="ion:person-sharp" href="/{loggedUser.id}" link="Profile"/>
+                <hr class="my-2">
+            </div>
+            <button on:click={handleSignOut} class="anchor">Log out</button>
+        </div>
+    {/if}
+{/if}
