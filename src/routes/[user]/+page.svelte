0f4d85de299adf1c5281042a8baa99ca6966d0d2
Martin <martin.damianof@gmail.com>
2025-06-08 14:34:12 +0200

    feat: draft publications in separate page in profile

--- src/routes/[user]/+page.svelte
+++ src/routes/[user]/+page.svelte
@@ -3,8 +3,9 @@
 	import type { LayoutData, PageServerData } from './$types';
     import { type Material, type Publication, PublicationType, type Tag, type User } from '@prisma/client';
 	import type { FetchedFileItem } from '$lib/database';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+    import type { ExtendedPublication } from '../api/publication/+server';
 
 	/* This is the data that was returned from the server */
 	export let data: LayoutData & PageServerData;
@@ -24,9 +25,19 @@
         publisher: User & {profilePicData: string};
     });
     let saved: publication[] = data.saved;
-    let posts : publication[] = data.publications.publications;
-
+    let posts = data.publications || [] as ExtendedPublication[];
     let tabSet: number = 0;
+
+
+    function transformPosts(posts: ExtendedPublication[]): publication[] {
+        return posts
+            .map((publication) => publication as publication)
+    }
+
+
+    let cardPosts = transformPosts(posts).filter(x => !x.isDraft);
+    let cardDrafts = transformPosts(posts).filter(x => x.isDraft);
+
 
     const getEncapsulatingType = (publication: any): string => {
         if(publication.type === PublicationType.Material) return publication.materials.encapsulatingType;
@@ -40,13 +51,16 @@
 <UserProfileBar user={user} userPhotoUrl={profilePic.data} />
 
 <div class="col-span-8">
-    {#if $page.data.session?.user.id === user.id}
+    {#if page.data.session?.user.id === user.id}
         <TabGroup justify="justify-center" class="col-span-8 lg:col-span-full">
             <Tab bind:group={tabSet} name="tab1" value={0}>
                 <p>Saved Publications</p>
             </Tab>
             <Tab bind:group={tabSet} name="tab2" value={1}>
                 <p>Your Publications</p>
+            </Tab>
+            <Tab bind:group={tabSet} name="tab3" value={2}>
+                <p>Draft Publications</p>
             </Tab>
             <svelte:fragment slot="panel">
                 {#if tabSet === 0}
@@ -69,7 +83,7 @@
                         <p class="col-span-2 text-center">So empty... There are no publications here </p>
                     {:else}
                         <div class="grid grid-cols-2 gap-4">
-                        {#each posts as publication, i}
+                        {#each cardPosts as publication, i}
                             <div class="col-span-1">
                             <PublicationCard imgSrc={publication.coverPicData}
                                              publication={publication}
@@ -80,6 +94,24 @@
                                              materialType={getEncapsulatingType(publication)}/>
                             </div>
                         {/each}
+                        </div>
+                    {/if}
+                {:else if tabSet === 2}
+                    {#if posts.length === 0}
+                        <p class="col-span-2 text-center">You don't have any draft publications</p>
+                    {:else}
+                        <div class="grid grid-cols-2 gap-4">
+                            {#each cardDrafts as publication, i}
+                                <div class="col-span-1">
+                                    <PublicationCard imgSrc={publication.coverPicData}
+                                                     publication={publication}
+                                                     liked={liked.includes(publication.id)}
+                                                     courses={posts[i].usedInCourse.map(x => x.course)}
+                                                     saved={data.savedByUser.includes(publication.id)}
+                                                     publisher={publication.publisher}
+                                                     materialType={getEncapsulatingType(publication)}/>
+                                </div>
+                            {/each}
                         </div>
                     {/if}
                 {/if}
@@ -93,7 +125,7 @@
                 <p class="col-span-full text-center">So empty... There are no publications here</p>
             {:else}
                 <div class="grid grid-cols-2 gap-4">
-                {#each posts as publication, i}
+                {#each cardPosts as publication, i}
                     <PublicationCard imgSrc={publication.coverPicData}
                                      publication={publication}
                                      liked={liked.includes(publication.id)}
