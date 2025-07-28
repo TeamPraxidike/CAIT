c2cfc6ee7719cdf6e1c167014d37ddf6cd856948
Martin <martin.damianof@gmail.com>
2025-07-28 18:29:59 +0200

    refactor: extract courses as a separate button component

--- src/lib/components/publication/SelectCourse.svelte
+++ src/lib/components/publication/SelectCourse.svelte
@@ -2,8 +2,12 @@
 	import type { Course } from '$lib/database/courses';
 	import CourseModal from '$lib/components/publication/CourseModal.svelte';
 	import { invalidate } from '$app/navigation';
+	import { goto } from '$app/navigation';
 	import { getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 	import { createEventDispatcher } from 'svelte';
+	import Icon from '@iconify/svelte';
+	import BrowseCourses from '$lib/components/publication/courses/BrowseCourses.svelte';
+	import CourseButton from '$lib/components/publication/courses/CourseButton.svelte';
 	const dispatch = createEventDispatcher();
 
 
@@ -13,9 +17,9 @@
 	export let courses: Course[] = [];
 	export let selectedCourseId: number | null = null;
 
-	function selectType(courseId: number) {
-		selectedCourseId = selectedCourseId === courseId ? null : courseId;
-	}
+	let showMyCourses = true;
+
+
 
 	const modal: ModalSettings = {
 		type: 'confirm',
@@ -33,7 +37,6 @@
 			}
 		}
 	};
-	console.log(courses)
 
 
 	let showModal = false;
@@ -57,53 +60,52 @@
 	<!--	<CourseModal existingCourse={null} onSuccess={refresh} close={closeModal} />-->
 	<!--{/if}-->
 
+	{#if showMyCourses}
+		{#if Array.isArray(courses) && courses.length > 0}
+			{#each courses as course}
+				<CourseButton
+					bind:course
+					bind:selectedCourseId
+					modalStore={modalStore}
+					modal={modal} />
 
-	{#if Array.isArray(courses) && courses.length > 0}
-		{#each courses as course}
-			<button
-				type="button"
-				on:click={() => selectType(course.id)}
-				class="group relative px-2 py-1 text-sm font-medium
-		   transition hover:font-bold
-		   {course.id === selectedCourseId ? 'bg-primary-600 text-white border-primary-500 rounded-full' : 'bg-white text-gray-800'}"
-			>
-				{course.courseName}
-				<button
-					type="button"
-					class="absolute -top-1 -right-1 opacity-0 group-hover:opacity-100 bg-red-500 text-white rounded-full w-4 h-4 flex items-center justify-center text-xs cursor-pointer"
-					aria-label="Delete course"
-					on:click={() => modalStore.trigger(modal)}
-				>
-					x
+				{#if course !== courses[courses.length - 1]}
+					<div class="w-px h-5 bg-gray-300 self-center"></div>
+				{/if}
+			{/each}
+		{:else}
+			<p>No courses available. Click button to add one.</p>
+		{/if}
+
+
+		<div class="flex gap-2 flex-wrap">
+			{#if courses.length === 0}
+				<button type="button" class="bg-primary-600 hover:bg-primary-700 text-white font-semibold py-2 px-4 rounded-xl shadow-sm transition"
+						on:click={() => openNewCourseModal()}>
+					Add a course
 				</button>
-			</button>
-			{#if course !== courses[courses.length - 1]}
-				<div class="w-px h-5 bg-gray-300 self-center"></div>
+			{:else}
+				<button type="button" name="add_maintainer" class="btn rounded-lg hover:bg-opacity-85 text-center"
+						on:click={() => openNewCourseModal()}>
+					<Icon icon="mdi:plus-circle" width="32" height="32"
+						  class="bg-surface-0 text-surface-800 hover:text-surface-600" />
+				</button>
 			{/if}
-		{/each}
-	{:else}
-		<p>No courses available. Click button to add one.</p>
-	{/if}
 
-
-	<div class="flex gap-2 flex-wrap">
-		<!--{#if courses.length === 0}-->
-			<button type="button" class="bg-primary-600 hover:bg-primary-700 text-white font-semibold py-2 px-4 rounded-xl shadow-sm transition"
-					on:click={() => openNewCourseModal()}>
-				Add a course
+			<button class="bg-primary-600 hover:bg-primary-700 text-white font-semibold py-2 px-4 rounded-xl shadow-sm transition"
+					type="button"
+					on:click={() => showMyCourses = !showMyCourses}>
+				Browse courses
 			</button>
-		<!--{:else}-->
-		<!--	<button type="button" name="add_maintainer" class="btn rounded-lg hover:bg-opacity-85 text-center"-->
-		<!--			on:click={() => window.location.href = '/course/create'}>-->
-		<!--		<Icon icon="mdi:plus-circle" width="32" height="32"-->
-		<!--			  class="bg-surface-0 text-surface-800 hover:text-surface-600" />-->
-		<!--	</button>-->
-		<!--{/if}-->
+		</div>
+	{:else}
+		<BrowseCourses bind:selectedCourseId={selectedCourseId}/>
 		<button class="bg-primary-600 hover:bg-primary-700 text-white font-semibold py-2 px-4 rounded-xl shadow-sm transition"
-				on:click={() => window.open('/courses/browse')}>
-			Browse courses
+				type="button"
+				on:click={() => showMyCourses = !showMyCourses}>
+			Show my courses
 		</button>
-	</div>
+	{/if}
 
 
 </div>
