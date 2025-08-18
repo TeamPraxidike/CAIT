f892c99605e0dd6cd20434bcc35acfc330922ac1
Yoan Popov <Y.Popov@student.tudelft.nl>
2025-08-18 19:05:27 +0200

    Feature: add prisma optimize

--- src/lib/database/prisma.ts
+++ src/lib/database/prisma.ts
@@ -1,3 +1,11 @@
 import { PrismaClient } from './prismaClient';
+//import { withOptimize } from "@prisma/extension-optimize";
 
-export const prisma: PrismaClient = new PrismaClient();
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const { withOptimize } = require('@prisma/extension-optimize');
+
+export const prisma: PrismaClient = new PrismaClient().$extends(
+	withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
+);
