ecb5980ef893877a8b82a6679f8548cf2f2d988e
Yoan Popov <Y.Popov@student.tudelft.nl>
2025-08-20 18:39:16 +0200

    Chore: remove prisma optimization due to 409 conflicts

--- src/lib/database/prisma.ts
+++ src/lib/database/prisma.ts
@@ -1,11 +1,15 @@
 import { PrismaClient } from './prismaClient';
+
+//TODO: this causes 409 conflicts with the remote dashboard. Could kill the entire process if not handled
 //import { withOptimize } from "@prisma/extension-optimize";
 
-import { createRequire } from 'module';
+// import { createRequire } from 'module';
+//
+// const require = createRequire(import.meta.url);
+// const { withOptimize } = require('@prisma/extension-optimize');
+//
+// export const prisma: PrismaClient = new PrismaClient().$extends(
+// 	withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
+// );
 
-const require = createRequire(import.meta.url);
-const { withOptimize } = require('@prisma/extension-optimize');
-
-export const prisma: PrismaClient = new PrismaClient().$extends(
-	withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY })
-);
+export const prisma: PrismaClient = new PrismaClient();
