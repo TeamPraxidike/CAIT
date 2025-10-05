2665889f8136107790969b99bad607e2a26da905
martin <martin.damianof@gmail.com>
2025-10-05 16:24:14 +0200

    chore: kinda working material edit page

---
+++ src/routes/[user]/[publication]/edit/material/+page.server.ts
@@ -0,0 +1,176 @@
+import type { Actions, PageServerLoad } from './$types';
+import {
+	type CircuitForm,
+	type FetchedFileArray,
+	type FetchedFileItem,
+	type MaterialForm, type UploadMaterialFileFormat
+} from '$lib/database';
+import type { Difficulty, Tag } from '@prisma/client';
+import { convertMaterial } from '$lib/util/types';
+import type { Course } from '$lib/database/courses';
+import { env } from '$env/dynamic/public';
+
+export const load: PageServerLoad = async ({ fetch, parent, locals }) => {
+	await parent();
+	const tagRes = await fetch('/api/tags');
+	const tags: Tag[] = await tagRes.json();
+	const usersRes = await fetch('/api/user');
+	const { users } = await usersRes.json();
+	const courses: Course[] = await (await fetch(`/api/course/user/${locals.user?.id}`)).json();
+	return { tags, users, courses, PUBLIC_SUPABASE_URL: env.PUBLIC_SUPABASE_URL };
+};
+
+export const actions = {
+	edit: async ({ request, fetch, params }) => {
+		const data = await request.formData();
+
+		const userId = data.get('userId')?.toString() || '';
+		const mid = data.get('materialId')?.toString() || '';
+
+		const title = data.get('title')?.toString() || '';
+		const description = data.get('description')?.toString() || '';
+		const isMaterial = data.get('isMaterial')?.toString() || '';
+		const difficulty = data.get('difficulty')?.toString() || '';
+		const copyright = data.get('copyright')?.toString() || '';
+		const time = data.get('timeEstimate')?.toString() || '';
+		const theoryApp = data.get('theoryAppRatio')?.toString() || '';
+		const selectedTags = data.get('tags')?.toString() || '';
+		const isDraft = data.get('isDraft')?.toString()  === 'true';
+
+		//I need to get the separate strings here so I can create them as string[], but not sure how to do that
+		const newTags = data.getAll('newTags') || '';
+
+		const maintainers = data.get('maintainers')?.toString() || '';
+
+		const prior = data.get('PK')?.toString() || '';
+		const LOs = data.get('learning_objectives')?.toString() || '';
+
+		const publisherId = data.get('publisherId')?.toString() || '';
+
+		const newTagsS = JSON.stringify(newTags);
+		const outerArray = JSON.parse(newTagsS);
+		const newTagsArray: string[] = JSON.parse(outerArray[0]) || [];
+
+		if (newTagsArray.length !== 0) {
+			const resTags = await fetch('/api/tags', {
+				method: 'POST',
+				body: JSON.stringify({ tags: newTagsArray }),
+			});
+			if (resTags.status !== 200) {
+				return {
+					status: resTags.status,
+					message: await resTags.json(),
+				};
+			}
+		}
+		if (userId === undefined) {
+			throw new Error('User Id was undefined');
+		}
+
+		/**
+		 * New file DATA (File list)
+		 */
+		const fileURLs = JSON.parse(data.get("fileURLs")?.toString() || '');
+		// const fileList: FileList = data.getAll(
+		// 	'file',
+		// ) as unknown as FileList;
+		const fileListUnformated: string[] = data.getAll('file') as unknown as string[];
+		const fileList: UploadMaterialFileFormat[] = fileListUnformated.map(item => {
+			return JSON.parse(item) as UploadMaterialFileFormat
+		})
+
+		const oldFilesDataFormData = data.get('oldFilesData');
+		if (oldFilesDataFormData === null) {
+			return { status: 400, message: 'No old files provided' };
+		}
+
+		const oldFileData: FetchedFileArray = JSON.parse(
+			oldFilesDataFormData.toString(),
+		);
+
+		// const fileArray: File[] =
+		// 	fileList.length === 0 ? [] : Array.from(fileList);
+
+		const addInfo: { title: string; type: string; info: string }[] = [];
+
+		for (const file of fileList) {
+			// TODO: good edge case
+			//if (file.size === 0) continue;
+
+			// const arBuf = await file.arrayBuffer();
+			// const buffer = Buffer.from(arBuf);
+			// const base64String = buffer.toString('base64');
+
+			const index = oldFileData.findIndex(
+				(fData: FetchedFileItem) => fData.fileId === file.info,
+			);
+			if (index === -1) {
+				addInfo.push({
+					title: file.title,
+					type: file.type,
+					info: file.info,
+				});
+			} else {
+				oldFileData.splice(index, 1);
+			}
+		}
+
+		const deleteInfo = oldFileData.map((data: FetchedFileItem) => {
+			return {
+				path: data.fileId,
+			};
+		});
+
+
+		const coverPicFile = data.get('coverPicMat');
+		let coverPic = null;
+
+		if (coverPicFile instanceof File) {
+			const buffer = await coverPicFile.arrayBuffer();
+			const info = Buffer.from(buffer).toString('base64'); //correct
+			coverPic = {
+				type: coverPicFile.type,
+				info,
+			};
+		}
+
+		const material: MaterialForm & {
+			materialId: number,
+			publisherId: string
+		} = {
+			materialId: Number(mid),
+			publisherId: publisherId,
+			userId: userId,
+			metaData: {
+				title: title,
+				description: description,
+				difficulty:
+					(difficulty.toLowerCase() as Difficulty) || 'easy',
+				learningObjectives: JSON.parse(LOs),
+				prerequisites: JSON.parse(prior),
+				copyright: data.get('copyright')?.toString() || '',
+				timeEstimate: Number(time),
+				theoryPractice: Number(theoryApp),
+				tags: JSON.parse(selectedTags),
+				maintainers: JSON.parse(maintainers),
+				materialType: (data.getAll('type') as string[]).map((type) => convertMaterial(type)),
+				isDraft: isDraft,
+				fileURLs: fileURLs || [],
+				course: Number(data.get('course')?.toString())
+			},
+			coverPic,
+			fileDiff: {
+				add: addInfo,
+				edit: [],
+				delete: deleteInfo,
+			},
+		};
+
+		const res = await fetch('/api/material/' + params.publication, {
+			method: 'PUT',
+			body: JSON.stringify(material),
+		});
+
+		return { status: res.status };
+	},
+} satisfies Actions;
---
+++ src/routes/[user]/[publication]/edit/material/+page.svelte
@@ -0,0 +1,466 @@
+<script lang="ts">
+	import type { LayoutServerData } from '../../$types';
+	import type { ActionData, PageServerData } from './$types';
+	import {
+		type Difficulty,
+		type Publication,
+		PublicationType,
+		type Tag as PrismaTag,
+		type User
+	} from '@prisma/client';
+	import { CircuitComponent, DifficultySelection, Meta, TheoryAppBar } from '$lib';
+	import {
+		FileButton, getToastStore, ProgressRadial
+	} from '@skeletonlabs/skeleton';
+	import { base64ToFile, concatFileList } from '$lib/util/file';
+	import type { PublicationView } from '../../+layout.server';
+	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/state';
+	import MetadataLOandPK from '$lib/components/MetadataLOandPK.svelte';
+	import MantainersEditBar from '$lib/components/user/MantainersEditBar.svelte';
+	import { onMount } from 'svelte';
+	import type { FetchedFileArray, FetchedFileItem, NodeDiffActions } from '$lib/database';
+	import TagsSelect from "$lib/components/TagsSelect.svelte";
+	import { isMaterialDraft, validateMetadata } from '$lib/util/validatePublication';
+	import { SvelteFlowProvider } from '@xyflow/svelte';
+	import type { NodeInfo } from '$lib/components/circuits/methods/CircuitTypes';
+	import {
+		type FileTUSMetadata,
+	} from '$lib/util/indexDB';
+	import { allUploadsDone } from '$lib/util/file'
+	import Banner from '$lib/components/publication/Banner.svelte';
+	import UploadFilesForm from '$lib/components/publication/UploadFilesForm.svelte';
+	import SelectType from '$lib/components/publication/SelectType.svelte';
+	import SelectCourse from '$lib/components/publication/SelectCourse.svelte';
+	import { changeCourse } from '$lib/util/coursesLogic';
+	import type { SupabaseClient } from '@supabase/supabase-js';
+
+
+
+	// $: ({loggedUser} = data);
+	let loggedUser = page.data.loggedUser;
+	export let data: LayoutServerData & PageServerData;
+	let serverData: PublicationView = data.pubView;
+	let publication: Publication = serverData.publication;
+	let supabaseClient: any = page.data.supabase;
+	const supabaseURL: string = data.PUBLIC_SUPABASE_URL;
+
+	let tags: string[] = serverData.publication.tags.map(tag => tag.content);
+
+	let title = publication.title;
+	let description = publication.description;
+	let theoryApp:any;
+	let time:any;
+	let copyright:any;
+
+	let selectedType:string[];
+	let files: FileList = [];
+
+	let oldFiles: any;
+	let fetchedFiles: FetchedFileArray | [] = [];
+
+	let LOs: string[] = serverData.publication.learningObjectives;
+	let PKs: string[] = serverData.publication.prerequisites;
+	let course: number | null = null;
+	let difficulty: Difficulty = serverData.publication.difficulty;
+	type UserWithProfilePic = User & { profilePicData: string };
+	let liked: number[] = [];
+	let saved: number[] = [];
+
+	let maintainers:UserWithProfilePic[] = serverData.publication.maintainers;
+	let users: UserWithProfilePic[] = data.users
+	let browsingUsers = users.filter(x => !maintainers.map(y=>y.id).includes(x.id));
+
+	let fileURLs: string[] = data.pubView.publication.materials.fileURLs.map((x) => x.url);
+	let fileTUSMetadata: { [key: string] : FileTUSMetadata } = {}
+
+	let circuitKey = Date.now();
+
+	let circuitNodesPlaceholder: NodeInfo[] = [];
+	$: circuitNodesPlaceholder = circuitNodesPlaceholder;
+
+	let dataTransferPromise: Promise<void> | null = null;
+
+	async function downloadFileFromSupabase(f: FetchedFileItem, supabaseClient: SupabaseClient<any, 'public', any>): Promise<File>{
+		const { data: blob, error } = await supabaseClient.storage
+			.from("uploadedFiles")
+			.download(f.fileId)
+
+		if (error) {
+			console.error('Error downloading file from Supabase:', error.message);
+			throw error;
+		}
+
+		if (!blob) {
+			console.error('Download succeeded but the returned blob is null.');
+			return null;
+		}
+
+		return new File([blob], f.name, {
+			type: blob.type,
+		});
+	}
+
+	async function defineDataTransfer(): Promise<DataTransfer>{
+		const localDataTransfer = new DataTransfer();
+
+		for (const f of fetchedFiles){
+			fileTUSMetadata[f.name] = {
+				originalName: f.name,
+				generatedName: f.fileId,
+				isDone: true
+			}
+
+			const file = await downloadFileFromSupabase(f, supabaseClient)
+
+			localDataTransfer.items.add(file);
+		}
+
+		return localDataTransfer
+	}
+
+	let coverPicPromise: null | Promise<File> = null;
+	const defaultCoverPicturePath = "/defaultCoverPic/assignment.jpg"
+
+	onMount(async () => {
+		theoryApp = serverData.publication.materials.theoryPractice;
+		time = serverData.publication.materials.timeEstimate;
+		copyright = serverData.publication.materials.copyright;
+		selectedType = serverData.publication.materials.encapsulatingType;
+
+		fetchedFiles = await data.fetchedFiles;
+
+		dataTransferPromise = defineDataTransfer().then(dt => {
+			files = dt.files;
+		});
+
+		// TODO: (random?) figure out why the type is a string rather than a null
+		if ((typeof serverData.coverFileData.data === "string" && serverData.coverFileData.data != 'null') ||
+			(typeof serverData.coverFileData.data !== "string" && serverData.coverFileData.data != null)){
+			coverPicPromise = downloadFileFromSupabase(serverData.coverFileData, supabaseClient);
+		}
+	});
+
+	let previousCourse: number | null = null;
+	$: if (course !== previousCourse) {
+		({ course, previousCourse, LOs, PKs } = changeCourse(course, previousCourse, LOs, PKs, data.courses));
+	}
+
+	let allTypes: {id:string, content:string }[] = ["presentation", "code", "video", "assignment", "dataset", "exam"].map(x => ({id : '0', content : x})); //array with all the tags MOCK
+
+
+	async function chooseCover(e: Event) {
+		const input = e.target as HTMLInputElement;
+		if (!input.files?.length) return;
+
+		const file = input.files[0];
+
+		// Check if file is valid
+		if (file.type === 'image/jpeg' || file.type === 'image/png') {
+			return file;
+		} else {
+			toastStore.trigger({
+				message: 'Invalid file type, please upload a .jpg or .png file',
+				background: 'bg-warning-200'
+			});
+		}
+
+		// TODO
+		// If you add a picture and then remove it
+		// You cannot re-add it until you select another image (and remove it)
+		// this is a workaround, think of it as deleting some cache
+		input.value = '';
+	}
+
+	async function chooseCoverPromiseHandler(e: Event){
+		coverPicPromise = chooseCover(e);
+	}
+
+	let allTags: PrismaTag[] = data.tags;
+
+	export let form: ActionData;
+	const toastStore = getToastStore();
+
+	$: if (form?.status === 200) {
+		isSubmitting = false;
+		toastStore.trigger({
+			message: 'Publication Edited successfully',
+			background: 'bg-success-200',
+			classes: 'text-surface-900',
+		});
+		goto(`/${loggedUser.username}/${publication.id}`);
+	} else if (form?.status === 400) {
+		isSubmitting = false;
+		if (!(allUploadsDone(fileTUSMetadata, files))){
+			toastStore.trigger({
+				message: 'Some files are still being uploaded',
+				background: 'bg-warning-200'
+			});
+		}
+		else {
+			toastStore.trigger({
+				message: `Malformed information, please check your inputs: ${form?.message}`,
+				background: 'bg-warning-200',
+				classes: 'text-surface-900'
+			});
+		}
+	} else if (form?.status === 401) {
+		toastStore.trigger({
+			message: `Unauthorized! ${form?.message}`,
+			background: 'bg-error-200'
+		});
+	} else if (form?.status === 500) {
+		toastStore.trigger({
+			message: 'An error occurred, please try again later or contact support',
+			background: 'bg-error-200'
+		});
+	}
+
+	export function changeFilezone(e: Event) {
+		const eventFiles = (e.target as HTMLInputElement).files;
+		if (eventFiles) {
+			files = concatFileList(files, eventFiles);
+		}
+	}
+	let newTags: string[] = [];
+
+	const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+		const confirmation = confirm('Data will be lost. Are you sure you want to proceed?');
+
+		if (!confirmation) {
+			event.preventDefault();
+			return;
+		}
+
+	};
+
+	onMount(() => {
+		(async () => {
+			window.addEventListener('beforeunload', handleBeforeUnload);
+
+			return () => {
+				window.removeEventListener('beforeunload', handleBeforeUnload);
+			};
+		})();
+	});
+
+	const handleInputEnter = (event: KeyboardEvent) => {
+		if(event.key === 'Enter'){
+			event.preventDefault();
+		}
+	}
+
+	const locks: boolean[] = [false, false, false, false];
+	// $: locks[0] = isMaterial ? files?.length === 0 : false;
+	// $: locks[1] = title.length < 1 || description.length < 1 || (isMaterial && selectedType === "Select Type");
+	// $: locks[2] = tags.length < 1 || LOs.length < 1;
+
+	let metadata;
+	$: metadata = {
+		title,
+		description,
+		learningObjectives: LOs,
+		tags,
+		materialType: selectedType,
+		isDraft: false
+	};
+	$: fileLength = files?.length;
+	// it is missing something so it is a draft. First check if it is a material, then check if the metadata is valid (circuit)
+	let draft = serverData.publication.isDraft;
+	// user has marked as draft
+	let markedAsDraft = draft;
+	$: draft = isMaterialDraft(metadata, fileLength) || !validateMetadata(metadata);
+
+	let isSubmitting: boolean = false;
+</script>
+
+
+<Meta title={publication.title} description="CAIT" type="site" />
+
+
+<div class="col-span-full">
+	<Banner metadata={metadata} files={fileLength}
+			materialType={metadata.materialType}/>
+</div>
+
+<form method="POST"
+	  enctype="multipart/form-data"
+	  action="?/edit"
+	  class="col-span-full my-20"
+	  use:enhance={async ({ formData }) => {
+		isSubmitting = true;
+
+		if (locks[0] || locks[1] || locks[2]) {
+			toastStore.trigger({
+				message: "Please complete all required fields before submitting.",
+				background: "bg-warning-200"
+			});
+			isSubmitting = false;
+			return;
+		}
+
+		// apparently files are automatically appended to the form using the
+		// file key, so just remove it
+		formData.delete('file')
+
+		// check if all the file uploads (excluding cover picture) are done
+		if (!(allUploadsDone(fileTUSMetadata, files))){
+			// alert('Some files are still being uploaded');
+			isSubmitting = false;
+			return;
+		}
+
+		// Array.from(files).forEach(file => appendFile(formData, file, 'file'));
+		for (const f of files){
+			 let uploadFormat = {
+				title: f.name,
+				type: f.type,
+				info: fileTUSMetadata[f.name]['generatedName']
+			}
+			formData.append('file', JSON.stringify(uploadFormat));
+		}
+
+
+		formData.append('title', title);
+		formData.append('description', description);
+
+		//formData.append('oldFiles', JSON.stringify(oldFiles));
+		//formData.append('oldFilesData', JSON.stringify(serverData.fileData));
+		formData.append('oldFilesData', JSON.stringify(fetchedFiles));
+
+		formData.append('userId', page.data.session?.user.id.toString() || '');
+		formData.append('tags', JSON.stringify(tags));
+		formData.append('newTags', JSON.stringify(newTags))
+		formData.append('difficulty', difficulty);
+		formData.append('maintainers', JSON.stringify(maintainers.map(x=>x.id)));
+		formData.append('learning_objectives', JSON.stringify(LOs));
+		formData.append('PK', JSON.stringify(PKs));
+		formData.append('theoryAppRatio', JSON.stringify(theoryApp));
+		formData.append('timeEstimate', time);
+		formData.append('copyright', copyright);
+		formData.append('type', JSON.stringify(selectedType));
+
+		let coverPicMat = null
+		if (coverPicPromise !== null){
+			coverPicMat = await coverPicPromise;
+		}
+		formData.append('coverPicMat', coverPicMat);
+
+
+		// TODO It does have the circuit, typescript is just hating. FIX THIS
+		formData.append('circuitId', JSON.stringify(serverData.publication.circuit?.id || 0));
+		formData.append('materialId', JSON.stringify(serverData.publication.materials?.id || 0));
+		formData.append('publisherId', JSON.stringify(serverData.publication.publisherId));
+		formData.append("isDraft", JSON.stringify(markedAsDraft || draft));
+		formData.append('fileURLs', JSON.stringify(fileURLs));
+		formData.append('course', course ? course.toString() : 'null');
+    }}>
+
+	<h2 class="text-lg md:text-xl lg:text-2xl xl:text-3xl font-semibold">Edit Publication</h2>
+	<p>{serverData.publication.publisher.firstName}</p>
+
+	<hr class="my-10">
+
+	<div class="flex flex-col gap-2 pl-3">
+		<label for="title"> Title</label>
+		<input minlength="3" type="text" id="title" name="title" bind:value={title} on:keydown={handleInputEnter}
+			   class="rounded-lg dark:bg-surface-800 bg-surface-50 w-full text-surface-700 dark:text-surface-400 focus:ring-0 focus:border-primary-400">
+
+		<label for="description"> Description</label>
+		<textarea minlength="10" id="description" name="description" bind:value={description}
+				  class="rounded-lg h-40 resize-y dark:bg-surface-800 bg-surface-50 w-full text-surface-700 dark:text-surface-400 focus:ring-0 focus:border-primary-400"
+		></textarea>
+
+		<SelectType bind:selectedTypes={selectedType}/>
+		<hr class="m-2">
+		<SelectCourse bind:selectedCourseId={course} courses={data.courses}/>
+		<div class="flex gap-4 items-center mt-6">
+			<DifficultySelection bind:difficulty={difficulty} />
+		</div>
+		<div class="flex flex-col items-start">
+			<label for="practice"> Theory to Practice Ratio: <br></label>
+			<TheoryAppBar bind:value={theoryApp}/>
+		</div>
+		<div class="flex flex-col md:flex-row col-span-full items-center gap-4">
+			<div class="w-full md:w-1/2">
+				<label for="estimate">Time Estimate (in minutes):</label>
+				<input min="0" type="number" name="estimate" bind:value={time} on:keydown={handleInputEnter} placeholder="How much time do the materials take"
+					   class="rounded-lg dark:bg-surface-800 bg-surface-50 w-full text-surface-700 dark:text-surface-400 focus:ring-0 focus:border-primary-400">
+			</div>
+			<div class="w-full md:w-1/2">
+				<label for="copyright">Copyright License (<a href="https://www.tudelft.nl/library/support/copyright#c911762" target=”_blank”
+															 class="text-tertiary-700" > Check here how this applies to you</a>):</label>
+				<input type="text" name="copyright" on:keydown={handleInputEnter} bind:value={copyright} placeholder="Leave blank if material is your own"
+					   class="rounded-lg dark:bg-surface-800 bg-surface-50 w-full text-surface-700 dark:text-surface-400 focus:border-primary-400 focus:ring-0">
+			</div>
+		</div>
+	</div>
+
+	<MetadataLOandPK bind:LOs={LOs} bind:priorKnowledge={PKs} adding="{true}"/>
+	<MantainersEditBar publisher={serverData.publication.publisher} bind:additionalMaintainers={maintainers} bind:searchableUsers={browsingUsers} bind:users={users}  />
+
+	<div class="text-token w-full md:w-1/2 space-y-2 pl-3">
+		<TagsSelect allTags={allTags} bind:tags={tags} bind:newTags={newTags}/>
+	</div>
+
+
+	{#if dataTransferPromise !== null}
+		{#await dataTransferPromise}
+			<p class="my-8">Loading files...</p>
+		{:then dataTransferAwaited}
+			<div class="mt-8">
+				<UploadFilesForm
+					supabaseURL={supabaseURL}
+					integrateWithIndexDB={false}
+					fetchedFiles={fetchedFiles}
+					bind:fileTUSMetadata={fileTUSMetadata}
+					bind:supabaseClient={supabaseClient}
+					bind:fileURLs={fileURLs}
+					bind:files={files}/>
+			</div>
+		{:catch error}
+			<!--TODO: Change color-->
+			<p style="color: red">Error while loading files. Reload the page to try again</p>
+		{/await}
+	{/if}
+
+	<div class="mt-4">
+		{#if coverPicPromise !== null}
+			{#await coverPicPromise}
+				<p>Loading cover picture</p>
+			{:then awaitedCoverPic}
+				<label for="coverPhoto">Cover Picture:</label>
+				<img src={URL.createObjectURL(awaitedCoverPic)} alt="Cover of publication">
+				<FileButton on:change={chooseCoverPromiseHandler} name="coverPhoto">Upload File</FileButton>
+			{:catch error}
+				<p>Error while loading cover picture</p>
+			{/await}
+		{:else}
+			<label for="coverPhoto">Cover Picture:</label>
+			<img src={defaultCoverPicturePath} alt="Cover of publication">
+			<FileButton on:change={chooseCoverPromiseHandler} name="coverPhoto">Upload File</FileButton>
+		{/if}
+	</div>
+
+	{#if !draft }
+		<div class="flex flex-row justify-end items-center gap-2">
+			<p class="pl-3">Save as a draft: </p>
+			<input type="checkbox" bind:checked={markedAsDraft} class="toggle toggle-primary" />
+		</div>
+	{/if}
+
+	<div class="flex float-right gap-2 mt-4">
+		{#if isSubmitting}
+			<div class="mr-8">
+				<ProgressRadial font={12} width="w-10"/>
+			</div>
+		{:else}
+			<button type="submit" class="btn rounded-lg variant-filled-primary text-surface-50"
+					disabled={locks[0] || locks[1] || locks[2] || isSubmitting}>
+				Save Changes
+			</button>
+		{/if}
+		<button type="button" on:click={()=>{window.history.back()}} class=" flex-none float-right btn rounded-lg variant-filled-surface text-surface-50">Cancel</button>
+	</div>
+</form>
