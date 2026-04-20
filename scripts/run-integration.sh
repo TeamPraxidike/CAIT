081efd92ea645f33552bdf954a58ebeb70f20223
Yoan Popov <mamaegalabar4@gmail.com>
2026-04-20 17:14:59 +0200

    Chore: add waiting period for supavisor and analytics, increase timeout to 60s

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -28,23 +28,25 @@ wait_healthy() {
 }
 
 wait_healthy db
+wait_healthy analytics
+wait_healthy supavisor
 wait_healthy kong
 
 # ---------------------------------------------------------------------------
 # Set env vars for the SvelteKit app (THESE ARE NOT PROD SECRETS)
 # we can also include a .env.ci file for sveltekit, but it's not a priority
 # ---------------------------------------------------------------------------
-export DATABASE_URL="postgres://postgres.1:123@localhost:6543/postgres?pgbouncer=true"
+export DATABASE_URL=postgres://postgres.1:123@localhost:6543/postgres?pgbouncer=true
 export SERVICE_ROLE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIn0.PqdH6E8yzZhWwB_c9o9e4LjdYXDTbEf5tdAqbBIrzKQ
-export DIRECT_URL="postgres://postgres.1:123@localhost:5432/postgres"
-export PUBLIC_SUPABASE_URL="http://localhost:8000"
+export DIRECT_URL=postgres://postgres.1:123@localhost:5432/postgres
+export PUBLIC_SUPABASE_URL=http://localhost:8000
 export PUBLIC_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE
-export FILESYSTEM="SUPABASE"
+export FILESYSTEM=SUPABASE
 export SAML_METADATA_IDP_URL=https://dev-gibxq4rldhm2q1st.eu.auth0.com/samlp/metadata/A7xXQ9i7TlNy98eaPhNbQAczMFVLTLxq
 export SRAM_SAML_METADATA_IDP_URL=https://meta.sram.surf.nl/metadata/proxy_idp.xml
 export PUBLIC_SAML_IDP_DOMAIN=dev-gibxq4rldhm2q1st.eu.auth0.com
 export PUBLIC_SRAM_SAML_IDP_DOMAIN=sram.surf.nl
-export PUBLIC_ENVIRONMENT="dev"
+export PUBLIC_ENVIRONMENT=dev
 
 # ---------------------------------------------------------------------------
 # Build and start preview server
