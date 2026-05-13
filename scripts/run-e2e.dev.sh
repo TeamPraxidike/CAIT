08fe00c23306c3db4a48edc2ee171130e6da8725
Yoan Popov <mamaegalabar4@gmail.com>
2026-05-13 14:41:32 +0200

    Chore: cleanup and include the script inside package.json

--- scripts/run-e2e.dev.sh
+++ scripts/run-e2e.dev.sh
@@ -1,21 +1,4 @@
-##!/usr/bin/env bash
-#
-#DIR="$(cd "$(dirname "$0")" && pwd)"                                       # get the current directory
-#ENV=$(grep -v '^#' .env | xargs)                                           # load the environment variables
-#
-#
-## Check if we are in a CI environment
-#if [ -z "$CI" ]; then
-#  docker-compose up -d db-test                                              # start the database
-#  echo '🟡 - Waiting for database to be ready...'                            # wait for the database to be ready
-#  "$DIR"/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'  # wait for the database to be ready
-#else
-#  npx playwright install-deps                                                # install playwright dependencies
-#  npx playwright install                                                     # install playwright browsers for testing
-#fi
-#
-#npx prisma migrate dev --name init                                         # run the migrations
-#npx playwright test                                                            # run playwright tests
+#!/usr/bin/bash
 
 set -euo pipefail
 
