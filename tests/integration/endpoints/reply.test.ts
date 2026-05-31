42d97a98164976bec7dd2588a03cb17ce71d5c57
Lukas Zimmerhackl <lukas.zim@hotmail.com>
2026-05-31 12:51:52 +0200

    created reply.test.ts but have not had time to actually write tests yet lmao

---
+++ tests/integration/endpoints/reply.test.ts
@@ -0,0 +1,33 @@
+import { describe, it, expect } from 'vitest';
+import { apiTestingUrl } from '../setup';
+import { beforeEach } from 'node:test';
+import { createUniqueUser } from '../../utility/users';
+
+// describe('REPLY API', () => {
+//     describe('/api/reply', () =>{
+//         beforeEach(() => {
+//             let user = createUniqueUser();
+//             // let comment = createc
+//         })
+
+//         describe('GET', () => {
+
+//         });
+
+//         describe('POST', () => {
+//             it('should have implemented tests', async () => {
+//                 expect(true).toBe(false);
+//             })
+//         });
+//     });
+
+//     describe('/api/reply/[id]', () =>{
+//         describe('GET', () => {
+
+//         });
+
+//         describe('POST', () => {
+            
+//         });
+//     });
+// });
