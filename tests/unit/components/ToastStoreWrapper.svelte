e397dacaa3ddefbadac5f39beb2040661b0cefb8
martin <martin.damianof@gmail.com>
2026-07-06 13:01:26 +0200

    test: comment tests and wrapper for toasts

---
+++ tests/unit/components/ToastStoreWrapper.svelte
@@ -0,0 +1,15 @@
+<!--
+The toast store has to be initialized in a svelte environment.
+ To get around this we use this wrapper.
+ -->
+
+<script lang="ts">
+	import { initializeStores } from '@skeletonlabs/skeleton';
+
+	initializeStores();
+
+	export let component;
+	export let props = {};
+</script>
+
+<svelte:component this={component} {...props} />
