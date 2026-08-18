c0ff837a5ddaca674a132c77391f116f1cd58efe
Yoan Popov <mamaegalabar4@gmail.com>
2026-08-18 20:32:53 +0200

    feat: privacy, terms, cookies added to a legal route, anchored in footer, referenced on signin and inside cookie banner

---
+++ src/routes/legal/privacy/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { Meta } from '$lib';
+    // https://vite.dev/guide/assets#static-asset-handling
+    import content from './content.html?raw'
+</script>
+
+<Meta title="Privacy Statement" description="CAIT Privacy Statement" type="site" />
+
+<article class="col-span-full my-16 prose max-w-3xl mx-auto dark:prose-invert prose-a:text-blue-500">
+    {@html content}
+</article>
